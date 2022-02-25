02a3252aea5b2082c0bb5beda25b68689bbf68cf
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-02-25 00:26:28 -0500

    Caso 3: Crear una nueva carrera Duplicada (Rojo)

--- tests/test_carrera.py
+++ tests/test_carrera.py
@@ -14,7 +14,13 @@ class CarreraTestCase(unittest.TestCase):
     def test_crearCarrera(self):
         competidoresPrueba = [{'Nombre':'Pepito Perez', 'Probabilidad':0.5},\
                         {'Nombre':'Pepa Perez', 'Probabilidad':0.5}]
-
         carrera = self.eporra.crearCarrera("Mi primera carrera", False, competidoresPrueba)
         self.assertEqual(carrera, True)
+    
+    def test_crearCarreraDuplicada(self):
+        competidoresPrueba = [{'Nombre':'Pepito Perez', 'Probabilidad':0.5},\
+                        {'Nombre':'Pepa Perez', 'Probabilidad':0.5}]
+        carrera1 = self.eporra.crearCarrera("Mi primera carrera", False, competidoresPrueba)
+        carrera2 = self.eporra.crearCarrera("Mi primera carrera", False, competidoresPrueba)
+        self.assertEqual(carrera2, False)
 
