f6235da8727deea005b18013aed9b183b6fdb98d
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-02-24 22:23:13 -0500

    Caso 1: Dar el valor de la descripcion de la aplicación (Rojo)

--- tests/test_aplicacion.py
+++ tests/test_aplicacion.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.logica.eporra import EPorra
+from src.modelo.aplicacion import Applicacion
 from src.modelo.declarative_base import Session
 
 class AplicacionTestCase(unittest.TestCase):
@@ -8,3 +9,7 @@ class AplicacionTestCase(unittest.TestCase):
     def setUp(self):
         self.session = Session()
         self.eporra = EPorra()
+
+    def darAplicacion(self):
+        resultadoEsperado = self.eporra.darDescripcionAplicacion()
+        self.assertEqual(resultadoEsperado, "Descipcion de la aplicaion")
