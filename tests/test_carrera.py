7d5618a471dc689892d532f6209f033045659b30
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-02-25 20:21:08 -0500

    Caso 4: Nombre de carrera vacio (Rojo)

--- tests/test_carrera.py
+++ tests/test_carrera.py
@@ -8,19 +8,21 @@ class CarreraTestCase(unittest.TestCase):
     def setUp(self):
         self.session = Session()
         self.eporra = EPorra()
+        self.competidoresPrueba = [{'Nombre':'Pepito Perez', 'Probabilidad':0.5},\
+                        {'Nombre':'Pepa Perez', 'Probabilidad':0.5}]
 
         
 
     def test_crearCarrera(self):
-        competidoresPrueba = [{'Nombre':'Pepito Perez', 'Probabilidad':0.5},\
-                        {'Nombre':'Pepa Perez', 'Probabilidad':0.5}]
-        carrera = self.eporra.crearCarrera("Mi primera carrera", False, competidoresPrueba)
+        carrera = self.eporra.crearCarrera("Mi primera carrera", False, self.competidoresPrueba)
         self.assertEqual(carrera, True)
     
     def test_crearCarreraDuplicada(self):
-        competidoresPrueba = [{'Nombre':'Pepito Perez', 'Probabilidad':0.5},\
-                        {'Nombre':'Pepa Perez', 'Probabilidad':0.5}]
-        carrera1 = self.eporra.crearCarrera("Mi primera carrera", False, competidoresPrueba)
-        carrera2 = self.eporra.crearCarrera("Mi primera carrera", False, competidoresPrueba)
+        carrera1 = self.eporra.crearCarrera("Mi primera carrera", False, self.competidoresPrueba)
+        carrera2 = self.eporra.crearCarrera("Mi primera carrera", False, self.competidoresPrueba)
         self.assertEqual(carrera2, False)
+    
+    def test_crearCarreraNombreVacio(self):
+        carrera1 = self.eporra.crearCarrera("", False, self.competidoresPrueba)
+        self.assertFalse(carrera1)
 
