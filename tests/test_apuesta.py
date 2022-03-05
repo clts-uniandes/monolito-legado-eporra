28580d3f157555060b7bb7ca9def58140f37e103
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-03-05 03:06:01 -0500

    Caso 2: Crear una nueva apuesta sin dar valor de la misma (Azul)

--- tests/test_apuesta.py
+++ tests/test_apuesta.py
@@ -12,31 +12,23 @@ class ApuestaTestCase(unittest.TestCase):
     def setUp(self):
         self.session = Session()
         self.eporra = EPorra()
-    
-    def test_crearApuesta(self):
         apostadorApuesta = Apostador(nombre="Marco Martin")
         self.session.add(apostadorApuesta)
         self.session.commit()
         competidoresPrueba = [{'Nombre':'Carlos Casas', 'Probabilidad':0.5},\
-                        {'Nombre':'Carla Cueva', 'Probabilidad':0.5}]
-        idCarreraPrueba = self.eporra.crearCarrera("Mi carrera de apuesta", competidoresPrueba)
+                         {'Nombre':'Carla Cueva', 'Probabilidad':0.5}]
+        self.idCarreraPrueba = self.eporra.crearCarrera("Mi carrera de apuesta", competidoresPrueba)
+        self.eporra.crearCompetidor(self.idCarreraPrueba, "Carlos Casas", 0.5)
+        self.eporra.crearCompetidor(self.idCarreraPrueba, "Carla Cueva", 0.5)
+    
+    def test_crearApuesta(self):
         valorApuestaPrueba = 5.00
-        self.eporra.crearCompetidor(idCarreraPrueba, "Carlos Casas", 0.5)
-        self.eporra.crearCompetidor(idCarreraPrueba, "Carla Cueva", 0.5)
-        exito = self.eporra.crearApuesta("Marco Martin", idCarreraPrueba, valorApuestaPrueba, "Carlos Casas")
+        exito = self.eporra.crearApuesta("Marco Martin", self.idCarreraPrueba, valorApuestaPrueba, "Carlos Casas")
         self.assertTrue(exito)
     
     def test_crearApuestaSinValor(self):
-        apostadorApuesta = Apostador(nombre="Marco Martin")
-        self.session.add(apostadorApuesta)
-        self.session.commit()
-        competidoresPrueba = [{'Nombre':'Carlos Casas', 'Probabilidad':0.5},\
-                        {'Nombre':'Carla Cueva', 'Probabilidad':0.5}]
-        idCarreraPrueba = self.eporra.crearCarrera("Mi carrera de apuesta", competidoresPrueba)
         valorApuestaPrueba = None
-        self.eporra.crearCompetidor(idCarreraPrueba, "Carlos Casas", 0.5)
-        self.eporra.crearCompetidor(idCarreraPrueba, "Carla Cueva", 0.5)
-        fracaso = self.eporra.crearApuesta("Marco Martin", idCarreraPrueba, valorApuestaPrueba, "Carlos Casas")
+        fracaso = self.eporra.crearApuesta("Marco Martin", self.idCarreraPrueba, valorApuestaPrueba, "Carlos Casas")
         self.assertFalse(fracaso)
 
     
