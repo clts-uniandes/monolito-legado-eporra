c592dc029b3011b82e183444e5814a5542c1c2ca
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-03-05 02:09:00 -0500

    Caso 2: Crear una nueva apuesta sin dar valor de la misma (Rojo)

--- tests/test_apuesta.py
+++ tests/test_apuesta.py
@@ -26,6 +26,20 @@ class ApuestaTestCase(unittest.TestCase):
         exito = self.eporra.crearApuesta("Marco Martin", idCarreraPrueba, valorApuestaPrueba, "Carlos Casas")
         self.assertTrue(exito)
     
+    def test_crearApuestaSinValor(self):
+        apostadorApuesta = Apostador(nombre="Marco Martin")
+        self.session.add(apostadorApuesta)
+        self.session.commit()
+        competidoresPrueba = [{'Nombre':'Carlos Casas', 'Probabilidad':0.5},\
+                        {'Nombre':'Carla Cueva', 'Probabilidad':0.5}]
+        idCarreraPrueba = self.eporra.crearCarrera("Mi carrera de apuesta", competidoresPrueba)
+        valorApuestaPrueba = None
+        self.eporra.crearCompetidor(idCarreraPrueba, "Carlos Casas", 0.5)
+        self.eporra.crearCompetidor(idCarreraPrueba, "Carla Cueva", 0.5)
+        fracaso = self.eporra.crearApuesta("Marco Martin", idCarreraPrueba, valorApuestaPrueba, "Carlos Casas")
+        self.assertFalse(fracaso)
+
+    
     def tearDown(self):
         self.session.query(Apuesta).delete()
         self.session.query(Competidor).delete()
