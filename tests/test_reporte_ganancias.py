dd1be010eb17381bc67d47cd2f10c5e7d8863de5
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-03-10 22:26:58 -0500

    Caso 4: Generar reporte de ganancias de una carrera - Conexion con front (Azul)

--- tests/test_reporte_ganancias.py
+++ tests/test_reporte_ganancias.py
@@ -36,7 +36,7 @@ class ReporteGananciasTestCase(unittest.TestCase):
     def test_mostrarReporteGananciasCompetidorInvalido(self):
         resultado = self.eporra.darReporteGanancias(self.idCarreraPrueba, 0)
         self.assertEqual(resultado, False)
-
+        
     def test_mostrarReporteGananciasCarreraInvalida(self):
         competidorGanador = self.session.query(Competidor.id).filter(Competidor.nombre == "Carlos Casas").first()[0]
         resultado = self.eporra.darReporteGanancias(0, competidorGanador)
