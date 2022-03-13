75ae7d2282f97d6a01c400494a9aae996de1048f
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-03-13 00:20:05 -0500

    Caso: Refactor pruebas test_aplicacion con Faker (azul) - CIERREFUNCIONALIDAD

--- tests/test_aplicacion.py
+++ tests/test_aplicacion.py
@@ -3,12 +3,18 @@ import unittest
 from src.logica.eporra import EPorra
 from src.modelo.declarative_base import Session
 
+from faker import Faker
+
 class AplicacionTestCase(unittest.TestCase):
 
     def setUp(self):
         self.session = Session()
         self.eporra = EPorra()
-
+        self.dataFactory = Faker('es_MX')
+        Faker.seed(1991)
+        descripcion = self.dataFactory.unique.catch_phrase()
+    
     def test_darAplicacion(self):
         resultadoEsperado = self.eporra.darDescripcionAplicacion()
         self.assertEqual(resultadoEsperado, "Descripción de la aplicación")
+    
